6ae6ade9dd92d9ac069161566aaed11700db361e
TOMIO NAGANO <Temujin142857@gmail.com>
2023-03-21 20:48:55 -0400

    Did a bit of set up, decided connections should be class variables, to save time opening and closing it, keep that in mind security wise
    created private methods to send commands, public methods should use pregenerated or carefully controlled commands to call these methods.

--- src/Database.java
+++ src/Database.java
@@ -2,39 +2,70 @@ import java.io.*;
 import java.sql.*;
 import java.util.Scanner;
 
+//is it better to make on connection that stays open the entire time database is valid
+//or open a new on every time a command needs to be sent? that seems secure but slow
 public class Database {
     private final String[] sections= new String[]{"Constructors:","Commands to run to update Database:"};
     private String databaseName;
     private String user;
     private String password;
+    private Connection conn;
+    private Statement st;
 
     public Database(String databaseName, String user, String password){
         this.databaseName=databaseName;
         this.user=user;
         this.password=password;
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn = DriverManager.getConnection(databaseName, user, password);
+        } catch (SQLException e) {
+            System.out.println("connection failed");
+            e.printStackTrace();
+        }
+        try {
+            st = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    private void executeCommand(String command){
+        //something something security checks
+        //something something execute command
+    }
+
+    private String executeRequest(String command){
+        //something something security checks
+        //something something send request
+        //return result
+        return null;
+    }
+
 
     public void autoUpdate(){
         for (String section: sections) {
             try {
                 runSectionOfCommands(section);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void runSectionOfCommands(String section) throws ClassNotFoundException, SQLException, FileNotFoundException {
+    /**
+     * Runs a section of code from the .txt file
+     * @param section
+     * @throws FileNotFoundException
+     */
+    private void runSectionOfCommands(String section) throws FileNotFoundException {
         boolean read=false;
         File commandFile= new File("Database_Commands.txt");
         Scanner scanner=new Scanner(commandFile);
-        Class.forName("org.postgresql.Driver");
-        Connection conn = DriverManager.getConnection(databaseName, user, password);
-        Statement st = conn.createStatement();
         int i=0;
         while(scanner.hasNext()) {
             if (scanner.nextLine().equals(section)) {
