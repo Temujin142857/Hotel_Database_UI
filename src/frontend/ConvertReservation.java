d289c02d657a4ed4cd50c61ff45515de77dade36
mostefaiii <mostefaimeriem@gmail.com>
2023-04-12 18:05:44 -0400

    Update ConvertReservation.java

--- src/frontend/ConvertReservation.java
+++ src/frontend/ConvertReservation.java
@@ -10,6 +10,8 @@ public class ConvertReservation extends JFrame {
     private JTextField creditCardNumberField;
     private JLabel CVVLabel;
     private JTextField CVVField;
+    private JLabel expiryLabel;
+    private JTextField expiryField;
 
     Color mainblue = new Color(28,49,94);
     Color nugreen = new Color(34, 124, 112);
@@ -22,18 +24,44 @@ public class ConvertReservation extends JFrame {
         setLayout(new BorderLayout());
         getContentPane().setBackground(mainblue);
 
-        JPanel mainPanel = new JPanel(new FlowLayout());
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        JPanel cardInfoPanel = new JPanel();
-        JPanel cvvExpiryPanel = new JPanel(new GridLayout(1,2));
+        
+        JPanel cvvExpiryPanel = new JPanel(new GridLayout(5,2));
 
         reservationIDLabel = new JLabel("Enter reservation ID:");
         reservationIDField = new JTextField();
-        mainPanel.add(reservationIDLabel);
-        mainPanel.add(reservationIDField);
+        cvvExpiryPanel.add(reservationIDLabel);
+        cvvExpiryPanel.add(reservationIDField);
 
         creditCardNumberLabel = new JLabel("Enter credit card number:");
-        
+        creditCardNumberField = new JTextField();
+        cvvExpiryPanel.add(creditCardNumberLabel);
+        cvvExpiryPanel.add(creditCardNumberField);
+
+        CVVLabel = new JLabel("CVV:");
+        CVVField = new JTextField();
+        cvvExpiryPanel.add(CVVLabel);
+        cvvExpiryPanel.add(CVVField);
+
+        //i sincerely doubt it matters that much that we need to validate this,
+        //so it's staying a text field
+        expiryLabel = new JLabel("Expiry:");
+        expiryField = new JTextField();
+        cvvExpiryPanel.add(expiryLabel);
+        cvvExpiryPanel.add(expiryField);
+
+        JButton booktoReservButton = new JButton("Confirm booking");
+        cvvExpiryPanel.add(booktoReservButton);
+
+
+        mainPanel.add(cvvExpiryPanel);
+        add(mainPanel);
+
+        pack();
+        setVisible(true);
+
 
 
     }
